c164987355a4f971980254a7bbebd44ffc41908a
Ray Badua <rayjonathanbadua@gmail.com>
2023-06-17 11:47:06 -0700

    tests, functions

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // import inquirer from "inquirer";
-const fs = require('fs');
-const Shape = require('./lib/shapes')
+const fs = require("fs");
+const Validate = require('./lib/validate')
+const Shape = require("./lib/shapes");
+const Triangle = require("./lib/shapes");
+const Circle = require("./lib/shapes");
+const Square = require("./lib/shapes");
 
 const questions = [
   {
-    type: 'input',
-    message: 'Please input logo text (up to three characters).',
-    name: 'logoText',
+    type: "input",
+    message: "Please input logo text (up to three characters).",
+    name: "logoText",
   },
-]
-
-
-const shape = new Shape("", "", "#00bfff");
-shape.shapeColor;
+];
