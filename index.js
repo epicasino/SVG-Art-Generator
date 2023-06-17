bf19f5e4d244fb1b2c6288c3ae746af47f98963f
Ray Badua <rayjonathanbadua@gmail.com>
2023-06-17 14:17:20 -0700

    more comments

--- index.js
+++ index.js
@@ -10,16 +10,18 @@ const Triangle = require("./lib/shapes");
 const Circle = require("./lib/shapes");
 const Square = require("./lib/shapes");
 
+// array of questions for inquirer
 const questions = [
   {
     type: "input",
     message: "Please input logo text (up to three characters).",
     name: "logoText",
+    // validate function that will create Validate class instance to execute .validateText method- if false, throw error
     validate: (input) => {
       const validate = new Validate(input);
       if (validate.validateText()) {
         return true;
-      } else throw "incorrect input";
+      } else throw "Incorrect input, must be <= 3 characters.";
     },
   },
   {
@@ -27,49 +29,55 @@ const questions = [
     message:
       "What color would you like your text to be? (Color keywords and hexacode supported)",
     name: "logoTextColor",
+    // validate function that will create Validate class instance to execute .validateTextColor method- if false, throw error
     validate: (input) => {
       const validate = new Validate("", input);
       if (validate.validateTextColor()) {
         return true;
-      } else throw "incorrect color choice";
+      } else throw "Incorrect color choice, must be hexacode or color keyword.";
     },
   },
   {
     type: "list",
     message: "Choose logo shape",
     name: "logoShape",
-    choices: ["triangle", "circle", "square"],
+    choices: ["Triangle", "Circle", "Square"],
   },
   {
     type: "input",
     message:
       "What color would you like your shape to be? (Color keywords and hexacode supported)",
     name: "logoShapeColor",
+    // validate function that will create Validate class instance to execute .validateShapeColor method- if false, throw error
     validate: (input) => {
       const validate = new Validate("", "", input);
       if (validate.validateShapeColor()) {
         return true;
-      } else throw "incorrect color choice";
+      } else throw "Incorrect color choice, must be hexacode or color keyword.";
     },
   },
 ];
 
 inquirer.prompt(questions)
   .then((answers) => {
+    // deconstructed objects from answers
     const {logoText, logoTextColor, logoShape, logoShapeColor } = answers
 
-    switch(answers.logoShape) {
-      case "triangle":
+    // switch statement- checks value of logoShape
+    // <shape>SVG variable will have string value- string value is for shape tag in SVG file & inserted of <shape> variable's class arguments
+    // <shape> variable will create new instance of the shape with validated user inserted inside of class instance arguments,
+    switch(logoShape) {
+      case "Triangle":
         const triangleSVG = 'polygon points="150, 18 244, 182 56, 182"';
         const triangle = new Triangle(logoText, logoTextColor, logoShapeColor, triangleSVG);
         return triangle;
         break;
-      case "circle":
+      case "Circle":
         const circleSVG = 'circle cx="150" cy="100" r="80"';
         const circle = new Circle(logoText, logoTextColor, logoShapeColor, circleSVG);
         return circle;
         break;
-      case "square":
+      case "Square":
         const squareSVG = 'rect width="100%" height="100%"';
         const square = new Square(logoText, logoTextColor, logoShapeColor, squareSVG);
         return square;
@@ -78,6 +86,8 @@ inquirer.prompt(questions)
 
   })
   .then((result) => {
+    // takes returned value(s) of truthy switch case and will do .renderShape() & .renderText() methods in defined variables
+    // set variable to have SVG render as a template literal, along with returned values of .renderShape .renderText methods inside.
     const shapeRender = result.renderShape();
     const shapeTextRender = result.renderText();
     const fullSVGRender = `
@@ -91,10 +101,11 @@ inquirer.prompt(questions)
 
 </svg>
 `;
+    // write file to be name of logo and content to be fullSVGRender variable- success message will be in console, otherwise error will be logged.
     fs.writeFile(`./examples/${result.text}.svg`, fullSVGRender, (err) =>
       err
         ? console.log(err)
         : console.log(`Success! Generated ${result.text}.svg`)
     );
-  })
+  });
   
